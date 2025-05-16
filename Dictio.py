fc8f38b00a326b6b98dab86d59dc06f981a367a7
Putra48 <putra.setiawanbiak@gmail.com>
2025-05-16 20:55:26 +0700

    Update Dictio.py

--- Dictio.py
+++ Dictio.py
@@ -1,46 +1,46 @@
 # Latihan soal 1
-# data = {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
+data = {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
 
-# print("key value item")
+print("key value item")
 
-# for key, value in data.items():
-#     print(key, value, key)
+for key, value in data.items():
+    print(key, value, key)
 
 # Latihan soal 2
-# lista = ['red', 'green', 'blue']
-# listb = ['#FF0000', '#008000', '#0000FF']
+lista = ['red', 'green', 'blue']
+listb = ['#FF0000', '#008000', '#0000FF']
 
-# hasil = dict(zip(lista, listb))
+hasil = dict(zip(lista, listb))
 
-# print(hasil)
+print(hasil)
 
 # Latihan soal 3
-# nama_file = input("Masukkan nama file : ")
+nama_file = input("Masukkan nama file : ")
 
-# try:
-#     file = open(nama_file, 'r')
+try:
+    file = open(nama_file, 'r')
 
-#     email_count = {}
+    email_count = {}
 
-#     for line in file:
-#         if line.startswith("From "):
-#             parts = line.split()
-#             email = parts[1]
-#             if email in email_count:
-#                 email_count[email] += 1
-#             else:
-#                 email_count[email] = 1
+    for line in file:
+        if line.startswith("From "):
+            parts = line.split()
+            email = parts[1]
+            if email in email_count:
+                email_count[email] += 1
+            else:
+                email_count[email] = 1
 
-#     file.close()
+    file.close()
 
-#     email_count_urut = dict(sorted(email_count.items()))
+    email_count_urut = dict(sorted(email_count.items()))
 
-#     print(email_count_urut)
+    print(email_count_urut)
 
-# except FileNotFoundError:
-#     print("File tidak ditemukan. Pastikan nama file benar.")
-# except Exception as e:
-#     print("Terjadi kesalahan:", e)
+except FileNotFoundError:
+    print("File tidak ditemukan. Pastikan nama file benar.")
+except Exception as e:
+    print("Terjadi kesalahan:", e)
 
 # Latihan soal 4
 nama_file = input("Masukkan nama file: ")
